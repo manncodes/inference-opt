58da5e0685579778f8d622b459f25219b72523ac
Mann Patel <46739555+manncodes@users.noreply.github.com>
2025-03-23 12:15:00 -0700

    Create decoding module init

---
+++ inference_opt/decoding/__init__.py
@@ -0,0 +1,6 @@
+"""
+Decoding strategies including Speculative Decoding.
+"""
+
+from inference_opt.decoding.base_decoding import GreedyDecoder
+from inference_opt.decoding.speculative_decoding import SpeculativeDecoder
