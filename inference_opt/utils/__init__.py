ec1b89593019e3942a79e44735f2a52db9f4324e
Mann Patel <46739555+manncodes@users.noreply.github.com>
2025-03-23 12:15:15 -0700

    Create utils module init

---
+++ inference_opt/utils/__init__.py
@@ -0,0 +1,6 @@
+"""
+Utility functions for configuration and profiling.
+"""
+
+from inference_opt.utils.config import Config
+from inference_opt.utils.profiling import profile_memory, profile_latency, profile_throughput
