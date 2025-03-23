d11018de63ade9450255a1625a0a3066ac4a870c
Mann Patel <46739555+manncodes@users.noreply.github.com>
2025-03-23 12:14:54 -0700

    Create attention module init

---
+++ inference_opt/attention/__init__.py
@@ -0,0 +1,7 @@
+"""
+Attention mechanisms including Radix Attention and MLA.
+"""
+
+from inference_opt.attention.base_attention import MultiHeadAttention
+from inference_opt.attention.radix_attention import RadixAttention
+from inference_opt.attention.mla import MultiHeadLatentAttention
