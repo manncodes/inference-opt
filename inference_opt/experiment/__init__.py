81c292a5fff7f60ff17b6423bb54d927428a2529
Mann Patel <46739555+manncodes@users.noreply.github.com>
2025-03-23 12:15:20 -0700

    Create experiment module init

---
+++ inference_opt/experiment/__init__.py
@@ -0,0 +1,6 @@
+"""
+Experimental setups for ablation studies and benchmarking.
+"""
+
+from inference_opt.experiment.ablation import run_ablation_study
+from inference_opt.experiment.benchmark import benchmark_model
