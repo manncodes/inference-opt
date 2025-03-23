b0aa7d181148559215a3f36ac9f4ab65e5ce4e04
Mann Patel <46739555+manncodes@users.noreply.github.com>
2025-03-23 12:15:07 -0700

    Create model module init

---
+++ inference_opt/model/__init__.py
@@ -0,0 +1,9 @@
+"""
+Transformer models with different optimization techniques.
+"""
+
+from inference_opt.model.transformer import Transformer
+from inference_opt.model.transformer_with_radix import TransformerWithRadix
+from inference_opt.model.transformer_with_mla import TransformerWithMLA
+from inference_opt.model.transformer_with_speculative import TransformerWithSpeculative
+from inference_opt.model.transformer_with_all import TransformerWithOptimizations
